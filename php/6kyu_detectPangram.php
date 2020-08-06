6922290ecb8ce53308c48fc394fc3d0f74527853
Andreas Brett <github@abrett.de>
2020-08-07 01:52:16 +0200

    6kyu PHP

---
+++ php/6kyu_detectPangram.php
@@ -0,0 +1,10 @@
+<?php
+// https://www.codewars.com/kata/545cedaa9943f7fe7b000048/
+
+function detect_pangram($string) {
+  for ($i = 65; $i <= 90; $i++) {
+    if (!stristr($string, chr($i))) return false;
+  }
+  return true;
+}
+?>
