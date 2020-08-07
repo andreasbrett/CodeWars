1a5ac18d9df882363bdbeb9fd9d347c4aa948fdd
Andreas Brett <github@abrett.de>
2020-08-07 03:14:51 +0200

    re-format

--- php/6kyu_detectPangram.php
+++ php/6kyu_detectPangram.php
@@ -1,10 +1,12 @@
 <?php
+
 // https://www.codewars.com/kata/545cedaa9943f7fe7b000048/
 
-function detect_pangram($string) {
-  for ($i = 65; $i <= 90; $i++) {
-    if (!stristr($string, chr($i))) return false;
+  function detect_pangram($string) {
+    for ($i = 65; $i <= 90; $i++) {
+      if (!stristr($string, chr($i))) return false;
+    }
+    return true;
   }
-  return true;
-}
+
 ?>
