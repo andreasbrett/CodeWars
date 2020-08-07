4a1a0b953126d1c9ac1873f378c8cc687cd2f06d
Andreas Brett <github@abrett.de>
2020-08-07 03:18:28 +0200

    7kyu PHP

--- php/7kyu_jadenCasingStrings.php
+++ php/7kyu_jadenCasingStrings.php
@@ -3,9 +3,9 @@
   // https://www.codewars.com/kata/5390bac347d09b7da40006f6/
 
   function toJadenCase($string) {
-      $words = explode(' ', $string);
-      for ($idx = 0; $idx < sizeof($words); $idx++) $words[$idx] = strtoupper(substr($words[$idx], 0, 1)) . substr($words[$idx], 1);
-      return implode(' ', $words);
+    $words = explode(' ', $string);
+    foreach ($words as &$word) $word = ucfirst($word);
+    return implode(' ', $words);
   }
 
 ?>
